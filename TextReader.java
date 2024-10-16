6608ce6164ba94854417b345a7544d18adbbd5c7
TannerCantwell <tanner.cantwell@mcusd709.org>
2024-10-16 09:18:27 -0500

    finished

--- TextReader.java
+++ TextReader.java
@@ -12,6 +12,7 @@ public class TextReader {
         for (int i = 0; i < 10; i++) {
             cutText = cutText.substring(cutText.indexOf("giraffe") + 7);
         }
+        
         System.out.println(cutText.substring(0,123));
 
         // find task 3
@@ -36,8 +37,46 @@ public class TextReader {
             }
             previousChar = text.substring(ii, ii+1);
         }
-        System.out.println(text.substring(longestConsecutiveIndex + longestConsecutiveAmount));
+
+        System.out.println(text.substring(longestConsecutiveIndex + longestConsecutiveAmount, longestConsecutiveIndex + longestConsecutiveAmount + 105));
 
         // find task 4
+        int mostVowelsIndex = 0;
+        int mostVowelsAmount = 0;
+        int currentVowels = 0;
+
+        for (int iii = 0; iii < text.length() - 29; iii++) {
+            String currentSubstring = text.substring(iii, iii + 30);
+            for (int character = 0; character < currentSubstring.length(); character++) {
+                String currentChar = currentSubstring.substring(character, character + 1);
+                if (currentChar .equals("a") || currentChar .equals("e") || currentChar .equals("i") || currentChar .equals("o") || currentChar .equals("u")) {
+                    currentVowels++;
+                }
+            }
+            if (currentVowels > mostVowelsAmount) {
+                mostVowelsAmount = currentVowels;
+                mostVowelsIndex = iii;
+            }
+            currentVowels = 0;
+        }
+        
+        System.out.println(text.substring(mostVowelsIndex + 30, mostVowelsIndex + 110));
+
+        // finish
+        int palindromeIndex = 0;
+
+        for (int iiii = 0; iiii < text.length() - 10; iiii++) {
+            String currentSubstring = text.substring(iiii, iiii + 11);
+            String reversed = "";
+            for (int character = 0; character < currentSubstring.length(); character++) {
+                reversed = currentSubstring.substring(character, character + 1) + reversed;
+            }
+            if (reversed.equals(currentSubstring)) {
+                palindromeIndex = iiii;
+                break;
+            }
+        }
+
+        System.out.println(text.substring(palindromeIndex + 11, palindromeIndex + 17));
     }
 }
